7368023c3c028cb48aa15fc4300012b33ed2e2b5
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-04-18 23:41:08 +0300

    Update Video.jsx

--- frontend/src/pages/Home/sections/Video.jsx
+++ frontend/src/pages/Home/sections/Video.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
-
-const x =
-  'https://www.youtube.com/embed/Gk-dvXm9-dc?rel=0&autohide=0&color=white&iv_load_policy=3&vq=hd10';
+import { FiPlay } from 'react-icons/fi';
 
 export default function Video() {
-  function playVideo() {
-    document.getElementById('youtube').src =
-      'https://www.youtube.com/embed/Gk-dvXm9-dc?rel=0&autohide=0&color=white&iv_load_policy=3&vq=hd10&autoplay=1';
-  }
   return (
-    <div className="h-full w-full relative">
-      <iframe
-        className="h-full w-full"
-        src={x}
-        title="YouTube video player"
-        id="youtube"
-      ></iframe>
+    <div className="h-full w-full relative flex justify-center items-center">
+      <img
+        src="https://im5.ezgif.com/tmp/ezgif-5-6225a4bb7e.gif"
+        alt=""
+        className="w-full h-full object-cover absolute"
+      />
+      <div className="absolute text-gray-500 border border-gray-500 rounded-full p-5 flex items-center justify-center">
+        <FiPlay size={40} />
+      </div>
     </div>
   );
 }
