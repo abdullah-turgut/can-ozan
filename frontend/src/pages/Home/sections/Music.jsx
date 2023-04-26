adb6a25bd1bc7841378b6d59af725571a615792c
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-04-26 20:48:44 +0300

    Update Music.jsx

--- frontend/src/pages/Home/sections/Music.jsx
+++ frontend/src/pages/Home/sections/Music.jsx
@@ -1,44 +1,60 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Music({ albums, setAlbums }) {
+  const [indexAlbum, setIndexAlbum] = useState(0);
+
   function slideRight() {
     document.querySelector('#album-slider').scrollBy({
-      left: 500,
+      left: 300,
       behavior: 'smooth',
     });
+    if (indexAlbum > 0) {
+      setIndexAlbum((preVal) => preVal + 1);
+    }
   }
   function slideLeft() {
     document.querySelector('#album-slider').scrollBy({
-      left: -500,
+      left: -300,
       behavior: 'smooth',
     });
+    if (indexAlbum < albums.length) {
+      setIndexAlbum((preVal) => preVal + 1);
+    }
   }
 
-  console.log(albums);
+  const listAlbums = albums.map((album, i) => (
+    <img
+      src={album.images[0].url}
+      alt=""
+      className={
+        i == indexAlbum
+          ? 'w-[500px] h-[500px] snap-start object-cover rounded-3xl'
+          : 'w-[400px] h-[400px] snap-start object-cover rounded-3xl'
+      }
+    />
+  ));
+  console.log(listAlbums);
 
   return (
-    <div className="w-full mt-28">
-      <p className="px-[10%] text-5xl mb-24 font-serif font-semibold">Müzik</p>
+    <div className="w-full pt-28 h-screen relative">
+      <p className="px-[10%] text-5xl mb-10  font-serif font-semibold">Müzik</p>
       <div
-        className="bg-black mx-[10%] flex overflow-scroll scrollbar-none"
+        className="mx-[10%] flex items-end gap-x-20 snap-x overflow-scroll scrollbar-none"
         id="album-slider"
       >
-        <div className="min-w-[500px] h-5 bg-gray-500"></div>
-        <div className="min-w-[500px] h-5 bg-red-500"></div>
-        <div className="min-w-[500px] h-5 bg-blue-500"></div>
-        <div className="min-w-[500px] h-5 bg-green-500"></div>
-        <div
-          className="absolute left-20 cursor-pointer text-3xl"
-          onClick={slideLeft}
-        >
-          {'<'}
-        </div>
-        <div
-          className="absolute right-20 cursor-pointer text-3xl"
-          onClick={slideRight}
-        >
-          {'>'}
-        </div>
+        {listAlbums}
+      </div>
+      <div
+        className="absolute top-1/2 left-20 cursor-pointer text-3xl"
+        onClick={slideLeft}
+      >
+        {'<'}
+      </div>
+      <div
+        className="absolute top-1/2 right-20 cursor-pointer text-3xl"
+        onClick={slideRight}
+      >
+        {'>'}
       </div>
     </div>
   );
